f89a31721a48fdfa65d47acdeb57b3d7efe805b5
E <effyverse@gmail.com>
2022-01-24 15:56:17 -0500

    set up application controller

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,5 @@
+class DashboardController < ApplicationController
+  def index
+    @hello = "Hello!"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  # mapping get request for /dashboard to index action of DashboardController
+  get "/dashboard", to: "dashboard#index"
+
 end
