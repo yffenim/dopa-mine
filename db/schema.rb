6d025075458d32b804bb77608a204ee1808d2f1a
E <effyverse@gmail.com>
2022-01-25 12:03:22 -0500

    roll back last three database entries using rollback STEP=3 because I did not follow Rails convention in capitalizing my models

--- db/schema.rb
+++ db/schema.rb
@@ -10,30 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_25_165513) do
-
-  create_table "activities", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.integer "user_id"
-    t.integer "comment_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "dopamine_points", force: :cascade do |t|
-    t.integer "value"
-    t.integer "activity_id"
-    t.integer "comment_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "user"
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
